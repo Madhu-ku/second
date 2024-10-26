44998447d4081f103cfe0ab3b91c341b46d57546
Madhu-ku <madhuiiest@gmail.com>
2024-10-26 23:37:31 +0530

    Updated components or fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Services from "./components/Services";
+import Testimonial from "./components/Testimonial";
+import Timeline from "./components/Timeline";
+import Works from "./components/Works";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <Home/>
+    <Works/>
+    <Timeline/>
+    <Services/>
+    <Testimonial/>
+    <Contact/>
+    <Footer/>
+   
+    </>
+   
   );
 }
 
 export default App;
+
+
+ // {
+      //   "date": "16/07/2024",
+      //   "title": "XCrypto",
+      //   "description": "This is the sample description about the project details. The more details the more details and evenually more details because I don't have anything to write",
+      //   "url": "https://react-crypto-app-pi.vercel.app",
+      //   "imgSrc": "https://cdn.pixabay.com/photo/2017/12/12/12/44/bitcoin-3014614_960_720.jpg"
+      // },
